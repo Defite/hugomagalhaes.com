c28cfadeb4bf590cf8888bba67c5f907db3bb1b4
Hugo <hugomn@gmail.com>
2018-12-31 19:33:14 +0000

    Automated deployment to GitHub Pages: 5ba62a718be873766687999657fa199ded25f078

--- src/components/FixedContainer.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const FixedContainer = styled.div`
-    padding: ${props => props.theme.padding};
-    margin: ${props => props.theme.margin};
-    max-width: ${props => props.theme.maxWidth};
-`;
-
-export default FixedContainer;
